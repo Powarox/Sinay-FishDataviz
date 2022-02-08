fcfe409d407df25f14c54a109b3ca3d9b43bf59d
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-08 18:41:48 +0100

    Clean app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-    <!-- Google font api -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- Google font font-family -->
-    <link href="https://fonts.googleapis.com/css2?family=Cabin:wdth,wght@91.7,529&display=swap" rel="stylesheet">
-    <!-- Font Awesome Icon -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+    <nav class="navigation">
+        <section class="nav">
+            <!-- <router-link to="/" id="logo">
+                <img src="./assets/logo.png" alt="Logo">
+                Logo
+            </router-link> -->
+            <router-link to="/">Home</router-link>
+        </section>
+    </nav>
 
-    <div id="app">
-        <nav class="navigation">
-            <section class="nav">
-                <!-- <router-link to="/" id="logo">
-                    <img src="./assets/logo.png" alt="Logo">
-                    Logo
-                </router-link> -->
-                <router-link to="/">Home</router-link>
-            </section>
-        </nav>
-
-        <main class="main">
-            <router-view/>
-        </main>
-    </div>
+    <main class="main">
+        <router-view/>
+    </main>
 </template>
 
 <script>
@@ -52,7 +42,6 @@
         text-decoration: none;
         list-style-type: none;
         outline: none;
-        font-family: sans-serif;
     }
 /* --- Colors --- */
     :root {
