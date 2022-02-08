d2e7e45b0821e9a6a92c270ef44b61511b75764f
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-08 17:39:38 +0100

    Clean & prepare project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Google font api -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Google font font-family -->
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:wdth,wght@91.7,529&display=swap" rel="stylesheet">
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'App',
+        components: {
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* --- Reset Css --- */
+    *, ::before, ::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        list-style-type: none;
+        outline: none;
+        font-family: sans-serif;
+    }
+/* --- Colors --- */
+    :root {
+        --main-first-color: #41B883;
+        --main-second-color: #35495E;
+    }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+    }
 </style>
