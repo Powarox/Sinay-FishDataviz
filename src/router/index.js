dff5b71601e86f6965a2372b94eb671a083e49d4
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-08 17:41:29 +0100

    Create router

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import NotFound from '../views/NotFound.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            title: 'Sinay - Dataviz',
+        }
+    },
+    {
+        name: 'NotFound',
+        path: '/:pathMatch(.*)',
+        component: NotFound,
+        meta: {
+            title: '404 Not Found',
+        }
+    }
+]
+
+const router = createRouter({
+    history : createWebHashHistory(process.env.BASE_URL),
+    routes
+});
+
+router.afterEach((to) => {  // (to, from)
+    // console.log(from, to);
+    document.title = to.meta.title;
+})
+
+export default router;
